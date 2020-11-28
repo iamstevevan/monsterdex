cee1d44b4c5943a74c17c88a64f425690ec53b28
steve van wasshenova <steve.vanwasshenova@gmail.com>
2020-11-28 14:03:48 -0700

    cleaning up bad indenting and handling favicon requests so as to not call xml load twice

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,9 @@ app.get('/', function(req, res) {
 	    //always show player at base load
 	    var selected = 0
 	    var monsters = JSON.parse(xmlParser.toJson(String(contents)));
+		
 		console.log("Found monsters xml with " + monsters.monsters.monster.length + " monsters (including player)");
+		
 		if("items" in monsters.monsters.monster[selected]) {
 			console.log(monsters.monsters.monster[selected]["items"]);
 		}		
@@ -27,50 +29,46 @@ app.get('/', function(req, res) {
 })
 
 app.get('/(:id)', function(req, res, next) {
-	console.log("GET by ID : " + req.params.id);
+	// dont handle favicon requests
+	if(req.params.id != "favicon.ico"){
+		console.log("GET by ID : " + req.params.id);
 
-	var selected = req.params.id
-	var selected_monster = selected
+		var selected = req.params.id
+		var selected_monster = selected
 
-	// console.log(selected);
+		var a = fs.readFile('monsters.xml', 'utf8', function(err, contents) {
 
-	var a = fs.readFile('monsters.xml', 'utf8', function(err, contents) {
-	    //console.log(contents);
+		    var monsters = JSON.parse(xmlParser.toJson(String(contents)));
 
-	    var monsters = JSON.parse(xmlParser.toJson(String(contents)));
+			console.log("Found monsters xml with " + monsters.monsters.monster.length + " monsters (including player)");
 
-		console.log("Found monsters xml with " + monsters.monsters.monster.length + " monsters (including player)");
-		//console.log(typeof(monsters));
-
-		var abort = false;
-		console.log("Looking for " + selected)
-		for(let i = 0; i<monsters.monsters.monster.length && !abort; i++) {
-			
-			if(monsters.monsters.monster[i].id == selected ) {
-				console.log("Found selected monster index " + i)
-				selected = i
-				abort = true
+			var abort = false;
+			console.log("Looking for " + selected)
+			for(let i = 0; i<monsters.monsters.monster.length && !abort; i++) {
+				
+				if(monsters.monsters.monster[i].id == selected ) {
+					console.log("Found selected monster index " + i)
+					selected = i
+					abort = true
+				}
 			}
-		}
-		if(monsters.monsters.monster) {
-			console.log(monsters.monsters.monster);
-		}
-		//console.log("Sending back " + selected_monster + " @ " + selected);
-		// res.json(true)
-		res.render('', {
-			title: 'Monster Rolodex',
-			data: monsters.monsters.monster,
-			selected: selected,	//id
-			selected_monster: selected_monster,	//string
-			selected_stat:  "text"
-		})
-	});
+
+			console.log("Sending back " + selected_monster + " @ " + selected);
+
+			res.render('', {
+				title: 'Monster Rolodex',
+				data: monsters.monsters.monster,
+				selected: selected,	//id
+				selected_monster: selected_monster,	//string
+				selected_stat:  "text"
+			})
+		});
+	}
 
 })
 
-// EDIT projects POST ACTION
 app.post('/', function(req, res, next) {
-	console.log("GET " + req.params.id);
+	console.log("POST by ID : " + req.body.hidden_selected);
 	
 	var selected = req.body.hidden_selected
 	var selected_monster = selected
@@ -78,12 +76,10 @@ app.post('/', function(req, res, next) {
 	console.log(selected);
 
 	var a = fs.readFile('monsters.xml', 'utf8', function(err, contents) {
-	    //console.log(contents);
 
 	    var monsters = JSON.parse(xmlParser.toJson(String(contents)));
 
 		console.log("Found monsters xml with " + monsters.monsters.monster.length + " monsters (including player)");
-		//console.log(typeof(monsters));
 
 		console.log("Looking for " + selected)
 		for(let i = 0; i<  monsters.monsters.monster.length; i++) {
@@ -95,7 +91,6 @@ app.post('/', function(req, res, next) {
 			}
 		}
 
-		// res.json(true)
 		res.render('', {
 			title: 'Monster Rolodex',
 			data: monsters.monsters.monster,
